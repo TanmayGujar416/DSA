2f749f3e0e64afcfc0ab0b921a8a79a5707a0606
Tanmay Gujar <tanmaygujar731@gmail.com>
2026-07-26 00:39:11 +0530

    Implement Next Greater Element I solution skeleton

---
+++ Stack And Queue/Stack/496.next-greater-element-i.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=496 lang=cpp
+ *
+ * [496] Next Greater Element I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+
+    }
+};
+// @lc code=end
+
